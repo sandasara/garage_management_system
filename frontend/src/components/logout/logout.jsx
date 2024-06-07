dfb8e7efdeeeb7cc9a8ff458c198192931bd600c
sandasara <sandasarad@gmail.com>
2024-06-07 22:38:36 +0530

    Fetched master with login

--- frontend/src/components/logout/logout.jsx
+++ frontend/src/components/logout/logout.jsx
@@ -3,25 +3,25 @@ import { useEffect } from 'react';
 import { useUser } from '../../UserContext';
 
 const Logout = () => {
-  const { logout } = useUser();
-  const navigate = useNavigate();
-
-  const handleLogout = (e) => {
-    e.preventDefault();
-    console.log('Logout button clicked');
-    logout();
-    console.log('User logged out');
-    navigate('/login'); // Redirect to login page
+    const { logout } = useUser();
+    const navigate = useNavigate();
+  
+    const handleLogout = (e) => {
+      e.preventDefault();
+      console.log('Logout button clicked');
+      logout();
+      console.log('User logged out');
+      navigate('/'); // Redirect to main page
+    };
+  
+    return (
+      <button
+        onClick={handleLogout}
+        className="block py-4 px-6 hover:bg-gray-600 text-left w-full"
+      >
+        Log Out
+      </button>
+    );
   };
-
-  return (
-    <button
-      onClick={handleLogout}
-      className="block py-4 px-6 hover:bg-gray-600 text-left w-full"
-    >
-      Log Out
-    </button>
-  );
-};
 
 export default Logout;
